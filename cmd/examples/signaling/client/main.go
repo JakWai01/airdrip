8a52115f6b298f516f3e74b7484722c23fdbf23b
JakWai01 <jakob.waibel@web.de>
2021-08-28 13:16:03 +0200

    feat: Start implementing signaling client

--- cmd/examples/signaling/client/main.go
+++ cmd/examples/signaling/client/main.go
@@ -5,6 +5,22 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"strings"
+)
+
+type Opcode string
+
+const (
+	application  Opcode = "application"
+	acceptance   Opcode = "acceptance"
+	rejection    Opcode = "rejection"
+	ready        Opcode = "ready"
+	introduction Opcode = "introduction"
+	offer        Opcode = "offer"
+	answer       Opcode = "answer"
+	candidate    Opcode = "candidate"
+	exited       Opcode = "exited"
+	resignation  Opcode = "resignation"
 )
 
 type Application struct {
@@ -13,9 +29,36 @@ type Application struct {
 	Mac       string `json:"mac"`
 }
 
+type Acceptance struct {
+	Opcode string `json:"opcode"`
+}
+
 type Ready struct {
 	Opcode string `json:"opcode"`
 	Mac    string `json:"mac"`
+}
+
+type Offer struct {
+	Opcode  string `json:"opcode"`
+	Mac     string `json:"mac"`
+	Payload string `json:"payload"`
+}
+
+type Answer struct {
+	Opcode  string `json:"opcode"`
+	Mac     string `json:"mac"`
+	Payload string `json:"payload"`
+}
+
+type Introduction struct {
+	Opcode string `json:"opcode"`
+	Mac    string `json:"mac"`
+}
+
+type Candidate struct {
+	Opcode  string `json:"opcode"`
+	Mac     string `json:"mac"`
+	Payload string `json:"payload"`
 }
 
 // take flags for community and mac
@@ -24,6 +67,8 @@ func main() {
 	var mac = flag.String("mac", "123", "mac (identification string)")
 	var community = flag.String("community", "a", "community to join")
 	flag.Parse()
+
+	var partnerMac string
 
 	conn, err := net.Dial("tcp", *laddr)
 	if err != nil {
@@ -43,36 +88,183 @@ func main() {
 		panic(err)
 	}
 
-	var input [1024]byte
+	// enter loop here
+	for {
+		var input [1024]byte
 
-	o, err := conn.Read(input[0:])
-	if err != nil {
-		panic(err)
+		o, err := conn.Read(input[0:])
+		if err != nil {
+			panic(err)
+		}
+
+		message := string(input[0:o])
+
+		fmt.Println(message)
+
+		values := make(map[string]json.RawMessage)
+
+		err = json.Unmarshal([]byte(message), &values)
+		if err != nil {
+			panic(err)
+		}
+
+		switch Opcode(strings.ReplaceAll(string(values["opcode"]), "\"", "")) {
+		case acceptance:
+			fmt.Println("acceptance")
+
+			// We actually don't need to unmarshal here because acceptance only contains an opcode
+			byteArray, err := json.Marshal(Ready{Opcode: string(ready), Mac: *mac})
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(string(byteArray))
+
+			byteArray = append(byteArray, "\n"...)
+			_, err = conn.Write([]byte(byteArray))
+			if err != nil {
+				panic(err)
+			}
+
+			break
+		case introduction:
+			fmt.Println("introduction")
+
+			// We get the mac of our partner and store it
+			var opcode Introduction
+
+			err := json.Unmarshal([]byte(message), &opcode)
+			if err != nil {
+				panic(err)
+			}
+
+			partnerMac = opcode.Mac
+
+			// send offer
+			byteArray, err := json.Marshal(Offer{Opcode: string(offer), Mac: partnerMac, Payload: "Hello World"})
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(string(byteArray))
+
+			byteArray = append(byteArray, "\n"...)
+			_, err = conn.Write([]byte(byteArray))
+			if err != nil {
+				panic(err)
+			}
+
+			break
+		case offer:
+			fmt.Println("offer")
+
+			var opcode Offer
+
+			err := json.Unmarshal([]byte(message), &opcode)
+			if err != nil {
+				panic(err)
+			}
+
+			partnerMac = opcode.Mac
+
+			// send answer
+			byteArray, err := json.Marshal(Answer{Opcode: string(answer), Mac: partnerMac, Payload: "Answer"})
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(string(byteArray))
+
+			byteArray = append(byteArray, "\n"...)
+			_, err = conn.Write([]byte(byteArray))
+			if err != nil {
+				panic(err)
+			}
+
+			break
+		case answer:
+			fmt.Println("answer")
+
+			var opcode Answer
+
+			err := json.Unmarshal([]byte(message), &opcode)
+			if err != nil {
+				panic(err)
+			}
+
+			partnerMac = opcode.Mac
+
+			// send candidate
+			byteArray, err := json.Marshal(Candidate{Opcode: string(candidate), Mac: partnerMac, Payload: "Candidate"})
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(string(byteArray))
+
+			byteArray = append(byteArray, "\n"...)
+			_, err = conn.Write([]byte(byteArray))
+			if err != nil {
+				panic(err)
+			}
+
+			break
+		case candidate:
+			fmt.Println("candidate")
+
+			var opcode Candidate
+
+			err := json.Unmarshal([]byte(message), &opcode)
+			if err != nil {
+				panic(err)
+			}
+
+			partnerMac = opcode.Mac
+
+			// send candidate back
+			byteArray, err := json.Marshal(Candidate{Opcode: string(candidate), Mac: partnerMac, Payload: "Candidate 2"})
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(string(byteArray))
+
+			byteArray = append(byteArray, "\n"...)
+			_, err = conn.Write([]byte(byteArray))
+			if err != nil {
+				panic(err)
+			}
+
+			// check for candidates
+			break
+		}
+
 	}
 
-	fmt.Println()
-	fmt.Println(string(input[0:o]))
+	// Ready
+	// ready := Ready{Opcode: "ready", Mac: *mac}
 
-	ready := Ready{Opcode: "ready", Mac: *mac}
+	// byteArray, err = json.Marshal(ready)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	byteArray, err = json.Marshal(ready)
-	if err != nil {
-		panic(err)
-	}
+	// byteArray = append(byteArray, "\n"...)
+	// _, err = conn.Write([]byte(byteArray))
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	byteArray = append(byteArray, "\n"...)
-	_, err = conn.Write([]byte(byteArray))
-	if err != nil {
-		panic(err)
-	}
+	// o, err = conn.Read(input[0:])
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	o, err = conn.Read(input[0:])
-	if err != nil {
-		panic(err)
-	}
+	// fmt.Println()
+	// fmt.Println(string(input[0:o]))
 
-	fmt.Println()
-	fmt.Println(string(input[0:o]))
+	// Unmarshal answer to get it
 
-	select {}
+	// Offer
+	// offer := Offer{Opcode: "offer", Mac: , Payload: "Hallo Welt"}
 }
