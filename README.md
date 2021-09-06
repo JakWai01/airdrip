8c3815db719455ebbd35d4046c4ed3ab1e1463d0
JakWai01 <jakob.waibel@web.de>
2021-09-06 13:21:01 +0200

    feat: Update README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# They-re-Taking-the-Hobbits-to-Isengard
-Airdrop but in bad and written in Go. Please excuse the name, the one I originally wanted is already claimed. Maybe I'll get another idea at some point.
+# airdrip
+
+## About
+Airdrop but with more drip. Please excuse the name, the one I originally wanted is already claimed. Maybe I'll get another idea at some point.
+
+## Disclaimer
+This project is still in progress. It may take a while for me to finish it.
--- main.go
+++ main.go
@@ -5,34 +5,5 @@ import (
 )
 
 func main() {
-
-	// This needs to be implemented on client side
-
-	// config := webrtc.Configuration{
-	// 	ICEServers: []webrtc.ICEServer{
-	// 		{
-	// 			URLs: []string{"stun:stun.l.google.com:19302"},
-	// 		},
-	// 	},
-	// }
-
-	// pc, err := webrtc.NewPeerConnection(config)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// offer, err := pc.CreateOffer(nil)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println(offer)
-
-	// if err := pc.SetLocalDescription(offer); err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println(pc.LocalDescription().Type.String())
-
 	cmd.Execute()
 }
