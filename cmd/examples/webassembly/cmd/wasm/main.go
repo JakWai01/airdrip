153cb6829e6f75c1bc935f306d807df1baca20bb
JakWai01 <jakob.waibel@web.de>
2021-10-10 18:24:34 +0200

    refactor: Cleanup

--- cmd/examples/webassembly/cmd/wasm/main.go
+++ cmd/examples/webassembly/cmd/wasm/main.go
@@ -1,29 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"syscall/js"
 
 	"github.com/JakWai01/airdrip/pkg/signaling"
 )
 
-func prettyJson(input string) (string, error) {
-	var raw interface{}
-	if err := json.Unmarshal([]byte(input), &raw); err != nil {
-		return "", err
-	}
-	pretty, err := json.MarshalIndent(raw, "", "  ")
-	if err != nil {
-		return "", err
-	}
-
-	return string(pretty), nil
-}
-
 func startClient(community string, filename string, file []byte) {
 	client := signaling.NewSignalingClient()
-
 	go func() {
 		client.HandleConn("airdrip.herokuapp.com", community, filename, file)
 	}()
@@ -46,37 +31,13 @@ func jsonWrapper() js.Func {
 		file := args[2].String()
 
 		startClient(community, filename, []byte(file))
-
 		return "Client started"
 	})
 	return jsonFunc
 }
 
-// func jsonWrapper() js.Func {
-// 	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-// 		if len(args) != 1 {
-// 			// When returning a value from Go to Javascript,
-// 			// the ValueOf function will be used automatically by the compiler
-// 			// to convert the Go value to a Javascript value.
-// 			return "Invalid no. of arguments passed"
-// 		}
-// 		// This represents the first parameter passed from JavaScript.
-// 		inputJSON := args[0].String()
-// 		fmt.Printf("input %s\n", inputJSON)
-// 		pretty, err := prettyJson(inputJSON)
-// 		if err != nil {
-// 			fmt.Printf("unable to convert to json %s\n", err)
-// 			return err.Error()
-// 		}
-// 		return pretty
-// 	})
-// 	return jsonFunc
-// }
-
 func main() {
 	fmt.Println("Go Web Assembly")
-	// The jsonFunc which formats the JSON can be called from JavScript
-	// using the function name formatJSON
 	js.Global().Set("send", jsonWrapper())
 	<-make(chan bool)
 }
